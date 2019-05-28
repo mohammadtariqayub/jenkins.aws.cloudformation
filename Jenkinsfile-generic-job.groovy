ef96fb8022034f8b16825fea7a60d622b079da0e
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-05-28 21:54:05 +1000

    adding Jenkins generic file

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -13,21 +13,6 @@ pipeline {
                 sh "/root/.local/lib/aws/bin/aws s3 ls ${params.BucketName}"
             }
         }
-
-    stage ('ec2 creation') {
-    echo "ec2 to aws-disrd-poc"
-    build job: 'AWSEc2-windows-creation-CD',
-          parameters: [
-            string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
-            string(defaultValue: "deploy-moh-cli-test-amapoc-moh-private-web-3", description: 'Enter Stack Name', name: 'StackName')
-            string(defaultValue: "subnet-061b3d9a7f8b88210", description: 'Enter Subnet ID', name: 'SubnetId')
-            string(defaultValue: "sg-0accba28318e4819c", description: 'Enter Security Group', name: 'SecurityGroupIds')
-            string(defaultValue: "ami-0628ef1f10e34307d", description: 'Enter Image ID', name: 'ImageId')
-            string(defaultValue: "mohammad.ayub@industry.nsw.gov.au", description: 'Enter System Owners email address', name: 'SystemOwner')
-            string(defaultValue: "windows", description: 'Enter OS', name: 'OS')
-            string(defaultValue: "amapocmoh-4", description: 'Enter Host Name', name: 'HostName')
-            string(defaultValue: "arn:aws:sns:ap-southeast-2:820636345429:mohammad-ping-poc-topic", description: 'Enter SNS Topics', name: 'SNSTopicARN')
-            string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
-          ]
-  }   
 }
+
+   
