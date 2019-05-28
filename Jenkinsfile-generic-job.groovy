db3a777927f7a74878895a16282159c2142d409c
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-05-28 21:59:45 +1000

    adding Jenkins generic file

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -1,12 +1,24 @@
 pipeline {
     agent { label 'slave' }
 
-    stage('template copy'){
+    parameters {
+        string(defaultValue: "subnet-061b3d9a7f8b88210", description: 'Enter Subnet ID', name: 'SubnetId')
+        string(defaultValue: "ami-0fb7513bcdc525c3b", description: 'Enter Image ID', name: 'ImageId')
+        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
+    }
+    stages{
+        stage('template copy'){
             steps {
                 git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
-                sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
+                sh "cp single_instance_tariq.yaml /root/jenkins.aws.templates/single_instance_tariq.yaml"
             }
         }
+        stage('list s3 bucket'){
+            steps {
+                echo "Bucket Name: ${params.BucketName}"
+                sh "/root/.local/lib/aws/bin/aws s3 ls ${params.BucketName}"
+            }
+        }
+    }
 }
-
    
