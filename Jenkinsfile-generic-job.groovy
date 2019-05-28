5e41ee6b5823dbc7a8adcd9b562a618f5dc9a093
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-05-28 21:57:23 +1000

    adding Jenkins generic file

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -7,12 +7,6 @@ pipeline {
                 sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
             }
         }
-    stage('list s3 bucket'){
-            steps {
-                echo "Bucket Name: ${params.BucketName}"
-                sh "/root/.local/lib/aws/bin/aws s3 ls ${params.BucketName}"
-            }
-        }
 }
 
    
