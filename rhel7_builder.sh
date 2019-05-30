1e4d1db648b505ca7095b2c1d36d720d1830f2ba
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-05-30 10:51:08 +1000

    Jenkinsfile and single_instance for windows

---
+++ rhel7_builder.sh
@@ -0,0 +1,1 @@
+yum install -y zip unzip bind-utils traceroute tcpdump python-setuptools dos2unix lsof mlocate bash-completion telnet glibc.i686 wget nfs-utils nmap elinks traceroute tree psmisc sos sysstat aide mailx setools-console setroubleshoot
