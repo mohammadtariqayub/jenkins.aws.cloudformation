7a7a7e1e5bf2db12c37be9766530b9a3285c8eda
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-02 10:44:37 +1000

    adding scripts

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -4,6 +4,7 @@ pipeline {
     parameters {
         string(defaultValue: "subnet-061b3d9a7f8b88210", description: 'Enter Subnet ID', name: 'SubnetId')
         string(defaultValue: "ami-0fb7513bcdc525c3b", description: 'Enter Image ID', name: 'ImageId')
+        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
     }
     stages{
         stage('template copy'){
