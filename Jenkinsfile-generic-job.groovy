c301769f5d0dfe980137bd855260dd18442fd6ba
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-02 12:02:07 +1000

    adding scripts

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -1,17 +1,16 @@
 pipeline {
     agent { label 'slave' }
 
-        stage('parameter_check'){
-          def validBucketName = (params['BucketName'] ==~ /^(?!\s*$).+/)
-          if (!validBucketName) { error "Invalid parameter BucketName. Should [0-9]{12}!" }
-        }
+    parameters {
+        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
+    }
+    stages{
         stage('template copy'){
-          steps {
-           git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
-           sh "cp Jenkinsfile-windows.groovy /root/jenkins.aws.templates/Jenkinsfile-windows.groovy"
-          }
+            steps {
+                git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
+                sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
+            }
         }
-
         stage ('list s3 bucket'){
           steps {
           build job: 's3-list-CD',
@@ -20,5 +19,6 @@ pipeline {
              ]  
            }  
         }
-    
+    }
+        
 }
