bda4b68fcec8d0836e41fb5efb3dc536b6169069
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-02 11:09:40 +1000

    adding scripts

--- s3-list-CD.sh
+++ s3-list-CD.sh
@@ -2,5 +2,6 @@
             steps {
                 echo "Bucket Name: ${params.BucketName}"
                 echo "Bucket Name: ${env.BucketName}"
-                sh "/root/.local/lib/aws/bin/aws s3 ls "@{ParameterKey="BucketName"; ParameterValue=$env:BucketName}"""
+                sh "/root/.local/lib/aws/bin/aws s3 ls "@{ParameterKey="env.BucketName"; ParameterValue=$env:BucketName}""
+
             }
