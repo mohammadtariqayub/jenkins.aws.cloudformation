1f2e5b427ae6ffb22884652744565335da27e798
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-02 11:47:42 +1000

    adding scripts

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -1,23 +1,26 @@
-node ('slave') {
-  stage ('parameter_check') {
-    def validBucketName = (params['BucketName'] ==~ /^(?!\s*$).+/)
-    if (!validBucketName) { error "Invalid parameter BucketName. Should [0-9]{12}!" }
+pipeline {
+    agent { label 'slave' }
+    
+   stages{
+        stage('parameter_check'){
+          def validBucketName = (params['BucketName'] ==~ /^(?!\s*$).+/)
+          if (!validBucketName) { error "Invalid parameter BucketName. Should [0-9]{12}!" }
+        }
 
-    echo "parameter_check stage complete!!"
-  }
+        stage('template copy'){
+          steps {
+           git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
+           sh "cp Jenkinsfile-windows.groovy /root/jenkins.aws.templates/Jenkinsfile-windows.groovy"
+          }
+        }
 
-    stage('template copy'){
-     steps {
-         git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
-         sh "cp Jenkinsfile-windows.groovy /root/jenkins.aws.templates/Jenkinsfile-windows.groovy"
+        stage ('list s3 bucket'){
+          steps {
+          build job: 's3-list-CD',
+             parameters: [
+             string(name: 'BucketName', value: params['BucketName'])
+             ]  
+           }  
         }
     }
-  stage ('list s3 bucket'){
-      steps {
-      build job: 's3-list-CD',
-        parameters: [
-            string(name: 'BucketName', value: params['BucketName'])
-        ]  
-     }  
-   }
 }
