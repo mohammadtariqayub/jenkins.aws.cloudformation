a86f9a67742ce0a29ddc70d775de521263e9ae5b
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-17 11:31:35 +1000

    chnaging pipeline job

--- Jenkinsfile-generic-job.groovy
+++ Jenkinsfile-generic-job.groovy
@@ -1,26 +1,15 @@
-def validBucketName = (params['BucketName'] ==~ /^(?!\s*$).+/)
-
-pipeline {
-    agent { label 'slave' }
-
-    parameters {
-        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
-    }
-
-    stages{
-        stage('template copy'){
-            steps {
-                git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
-                sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
-            }
+agent { label 'slave' }
+  stage('template copy'){
+    steps {
+           git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
+           sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
         }
-        stage ('list s3 bucket'){
-          steps {
-          build job: 's3-list-CD',
+    }
+  stage ('list s3 bucket'){
+  echo "Deploying s3 list"
+  build job: 's3-list-CD',
              parameters: [
              string(name: 'BucketName', value: params['BucketName'])
              ]  
-           }  
-        }
-    }      
-}
+  }  
+            
