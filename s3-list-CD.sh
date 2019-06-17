44303a0da3155abd6fcdd5e2570b1248b3c204ad
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-17 10:21:00 +1000

    chnaging pipeline job

--- s3-list-CD.sh
+++ s3-list-CD.sh
@@ -1,9 +1,4 @@
-parameters {
-        string(defaultValue: "", description: 'K', name: 'HELLO')
-    }
-
 stage('list s3 bucket'){
     steps {
         sh 'echo ${env:BucketName}'
-        sh 'echo "Bucket Name: ${HELLO}"'
     }
