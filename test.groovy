f75fe408abbefab32dd355c8072fe57e11cef235
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-18 15:10:29 +1000

    chnaging pipeline job

--- test.groovy
+++ test.groovy
@@ -1,19 +1,15 @@
-   pipeline {
-        agent { label 'slave' }
-
-        parameters {
-            string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
-        }
-
-        stages {
-            stage('template copy'){
-            steps {
-                git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
-                sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
-                }
-            }
-            stage('list s3 bucket'){
-                    build 's3-list-CD'
-            }
-        }
-    }
+node ('slave') {
+  stage ('parameter_check') {
+    def validBucketName = (params['BucketName'] ==~ /^(?!\s*$).+/)
+    if (!validBusinessOwner) { error "Invalid parameter BusinessOwner. Parameter cannot be empty!" }
+    
+    echo "parameter_check stage complete!!"
+  }
+  
+  stage('list s3 bucket') {
+    echo "Listing s3 bucket"
+    build job: 's3-list-CD',
+           parameters: [
+                string(name: 'BucketName', value: params['BucketName'])
+            ]
+  }
