9d5e8cf8fab56d9ccbf549cc40fc1ed7ac3c5f52
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-18 15:33:34 +1000

    chnaging pipeline job

--- test.groovy
+++ test.groovy
@@ -1,21 +1,21 @@
 node ('slave') {
   stage ('parameter_check') {
-    def validBucketName = (params['BucketName'] ==~ /^(?!\s*$).+/)
-    if (!validBucketName) { error "Invalid parameter BusinessOwner. Parameter cannot be empty!" }
+    def validAWSBucketName = (params['AWSBucketName'] ==~ /^(?!\s*$).+/)
+    if (!validAWSBucketName) { error "Invalid parameter BusinessOwner. Parameter cannot be empty!" }
     
     echo "parameter_check stage complete!!"
   }
 
   stage('template copy'){
-            git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
-            sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
+    git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
+    sh "cp single_instance_generic.yaml /root/jenkins.aws.templates/single_instance_generic.yaml"
   }
 
   stage('list s3 bucket') {
     echo "Listing s3 bucket"
     build job: 's3-list-CD',
            parameters: [
-                string(name: 'BucketName', value: params['BucketName'])
+                string(name: 'BucketName', value: params['AWSBucketName'])
            ]
   }
 }
