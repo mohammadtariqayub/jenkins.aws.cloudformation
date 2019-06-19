69e301f2b3dab05b7b7c95577ab78f20e26597d1
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-19 15:55:23 +1000

    changing pipeline job

--- test.groovy
+++ test.groovy
@@ -1,25 +1,5 @@
 node ('slave') {
   
-  parameters {
-        string(defaultValue: "deploy-moh-cli-test-amapoc-moh-private-1", description: 'Enter Stack Name', name: 'StackName')
-        string(defaultValue: "amapocmoh-1", description: 'Enter Host Name', name: 'HostName')
-        string(defaultValue: "windows", description: 'Enter OS', name: 'OS')
-        string(defaultValue: "ami-0628ef1f10e34307d", description: 'Enter Image ID', name: 'ImageID')
-        string(defaultValue: "subnet-061b3d9a7f8b88210", description: 'Enter Subnet ID', name: 'SubnetId')
-        string(defaultValue: "t3.small", description: 'Enter Instance Type', name: 'InstanceType')
-        string(defaultValue: "test", description: 'Environment type. eg, prd', name: 'Environment')
-        string(defaultValue: "true", description: 'Specifiy is the instance should join  Active Directory domain.', name: 'JoinDomain')
-        string(defaultValue: "businesshours", description: 'A valid power schedule. eg, 247', name: 'GorillaStack')
-        string(defaultValue: "aws-disrd-poc", description: 'Name of the AWS Account associated with this instance', name: 'AWSAccount')
-        string(defaultValue: "BTSManagedInstanceProfile", description: 'BTSManagedInstanceProfile', name: 'InstanceProfile')
-        string(defaultValue: "DOI-POC-MOHAMMAD-ACCESS-KEY", description: 'Enter EC2 Key', name: 'KeyPairName')
-        string(defaultValue: "sg-0accba28318e4819c", description: 'Enter Security Group', name: 'SecurityGroupId')
-        string(defaultValue: "https://amaprdsymprxelb01.dpi.nsw.gov.au:8080", description: 'Proxy address.', name: 'Proxy')
-        string(defaultValue: "arn:aws:sns:ap-southeast-2:820636345429:mohammad-ping-poc-topic", description: 'Enter the SNS Topic ARN to subscribe to', name: 'SNSTopicARN')
-        string(defaultValue: "mohammad.ayub@industry.nsw.gov.au", description: 'Enter System Owners email address', name: 'SystemOwner')        
-        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
-  }
-    
   stage ('parameter_check') {
     def validAWSBucketName = (params['AWSBucketName'] ==~ /^(?!\s*$).+/)
     if (!validAWSBucketName) { error "Invalid parameter Bucket Name. Parameter cannot be empty!" }
