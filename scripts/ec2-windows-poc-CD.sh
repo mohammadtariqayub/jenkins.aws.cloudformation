b07d28109a30ec65ff559335ffdccbf05a33d350
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-19 22:29:03 +1000

    changing pipeline job

--- scripts/ec2-windows-poc-CD.sh
+++ scripts/ec2-windows-poc-CD.sh
@@ -1,10 +1,2 @@
-/root/.local/lib/aws/bin/aws cloudformation create-stack --stack-name $StackName \
---template-body file:///root/jenkins.aws.cloudformation/single_instance_windows-cutomized.yaml --parameters \
-ParameterKey=SubnetId,ParameterValue=$SubnetId ParameterKey=ImageID,ParameterValue=$ImageID \
-ParameterKey=SystemOwner,ParameterValue=$SystemOwner ParameterKey=OS,ParameterValue=$OS \
-ParameterKey=HostName,ParameterValue=$HostName ParameterKey=SecurityGroupId,ParameterValue=$SecurityGroupId \
-ParameterKey=InstanceType,ParameterValue=$InstanceType ParameterKey=InstanceProfile,ParameterValue=$InstanceProfile \
-ParameterKey=KeyPairName,ParameterValue=$KeyPairName ParameterKey=Environment,ParameterValue=$Environment \
-ParameterKey=AWSAccount,ParameterValue=$AWSAccount ParameterKey=GorillaStack,ParameterValue=$GorillaStack \
-ParameterKey=SNSTopicARN,ParameterValue=$SNSTopicARN ParameterKey=JoinDomain,ParameterValue=$JoinDomain \
-ParameterKey=Proxy,ParameterValue=$Proxy
+FinalStatus=`aws cloudformation describe-stacks --stack-name deploy-moh-cli-test-amapoc-moh-private-1 |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
+echo $FinalStatus
