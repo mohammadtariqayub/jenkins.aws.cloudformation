5c671486e4ab2497cde987f59335415ed7acc6ab
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-19 22:31:17 +1000

    changing pipeline job

--- scripts/ec2-windows-poc-CD.sh
+++ scripts/ec2-windows-poc-CD.sh
@@ -1,2 +1,2 @@
-FinalStatus=`aws cloudformation describe-stacks --stack-name deploy-moh-cli-test-amapoc-moh-private-1 |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
+FinalStatus=`/root/.local/lib/aws/bin/aws cloudformation describe-stacks --stack-name deploy-moh-cli-test-amapoc-moh-private-1 |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
 echo $FinalStatus
