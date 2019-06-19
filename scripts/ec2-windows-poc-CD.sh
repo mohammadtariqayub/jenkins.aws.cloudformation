6368f4271a92b308abafdc102b52fcb32a8f93d5
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-19 15:31:39 +1000

    changing pipeline job

---
+++ scripts/ec2-windows-poc-CD.sh
@@ -0,0 +1,10 @@
+/root/.local/lib/aws/bin/aws cloudformation create-stack --stack-name $StackName\
+--template-body file:///rroot/jenkins.aws.cloudformation/single_instance_windows-cutomized.yaml --parameters \
+ParameterKey=SubnetId,ParameterValue=$SubnetId} ParameterKey=ImageID,ParameterValue=$ImageID} \
+ParameterKey=SystemOwner,ParameterValue=$SystemOwner} ParameterKey=OS,ParameterValue=$OS} \
+ParameterKey=HostName,ParameterValue=$HostName} ParameterKey=SecurityGroupId,ParameterValue=$SecurityGroupId} \
+ParameterKey=InstanceType,ParameterValue=$InstanceType} ParameterKey=InstanceProfile,ParameterValue=$InstanceProfile} \
+ParameterKey=KeyPairName,ParameterValue=$KeyPairName} ParameterKey=Environment,ParameterValue=$Environment} \
+ParameterKey=AWSAccount,ParameterValue=${AWSAccount} ParameterKey=GorillaStack,ParameterValue=$GorillaStack} \
+ParameterKey=SNSTopicARN,ParameterValue=$SNSTopicARN} ParameterKey=JoinDomain,ParameterValue=$JoinDomain} \
+ParameterKey=Proxy,ParameterValue=$Proxy}
