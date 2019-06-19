dd7e20fad05021b51f47cdf72c1be23057ca67d3
Mohammad Tariq Ayub <mohammad.tariq.ayub@ioutlook.com>
2019-06-19 15:52:49 +1000

    changing pipeline job

--- test.groovy
+++ test.groovy
@@ -30,6 +30,7 @@ node ('slave') {
   stage('template copy'){
     git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
     sh "cp ./scripts/s3-list-CD.sh /root/jenkins.aws.cloudformation/scripts/s3-list-CD.sh"
+    sh "cp ./scripts/ec2-windows-poc-CD.sh /root/jenkins.aws.cloudformation/scripts/ec2-windows-poc-CD.sh"
     }
 
   stage('list s3 bucket') {
