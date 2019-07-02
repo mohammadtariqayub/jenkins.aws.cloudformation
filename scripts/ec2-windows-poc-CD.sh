1da571f260b67912440a5cb339a4c3333a114ed6
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-02 12:06:22 +1000

    modyfying ec2-windows-poc-CD.sh

--- scripts/ec2-windows-poc-CD.sh
+++ scripts/ec2-windows-poc-CD.sh
@@ -1,12 +1,12 @@
-sh "/root/.local/lib/aws/bin/aws cloudformation create-stack --stack-name ${params.StackName} \
+sh "/root/.local/lib/aws/bin/aws cloudformation create-stack --stack-name $StackName \
 --template-body file:///root/jenkins.aws.templates/single_instance_windows-cutomized.yaml --parameters \
-ParameterKey=SubnetId,ParameterValue=${params.SubnetId} ParameterKey=ImageID,ParameterValue=${params.ImageID} \
-ParameterKey=SystemOwner,ParameterValue=${params.SystemOwner} ParameterKey=OS,ParameterValue=${params.OS} \
-ParameterKey=HostName,ParameterValue=${params.HostName} ParameterKey=SecurityGroupId,ParameterValue=${params.SecurityGroupId} \
-ParameterKey=InstanceType,ParameterValue=${params.InstanceType} ParameterKey=InstanceProfile,ParameterValue=${params.InstanceProfile} \
-ParameterKey=KeyPairName,ParameterValue=${params.KeyPairName} ParameterKey=Environment,ParameterValue=${params.Environment} \
-ParameterKey=AWSAccount,ParameterValue=${params.AWSAccount} ParameterKey=GorillaStack,ParameterValue=${params.GorillaStack} \
-ParameterKey=SNSTopicARN,ParameterValue=${params.SNSTopicARN} ParameterKey=JoinDomain,ParameterValue=${params.JoinDomain} \
-ParameterKey=Proxy,ParameterValue=${params.Proxy}" 
+ParameterKey=SubnetId,ParameterValue=$SubnetId ParameterKey=ImageID,ParameterValue=$ImageID \
+ParameterKey=SystemOwner,ParameterValue=$SystemOwner ParameterKey=OS,ParameterValue=$OS \
+ParameterKey=HostName,ParameterValue=$HostName ParameterKey=SecurityGroupId,ParameterValue=$SecurityGroupId \
+ParameterKey=InstanceType,ParameterValue=$InstanceType ParameterKey=InstanceProfile,ParameterValue=$InstanceProfile \
+ParameterKey=KeyPairName,ParameterValue=$KeyPairName ParameterKey=Environment,ParameterValue=$Environment \
+ParameterKey=AWSAccount,ParameterValue=$AWSAccount ParameterKey=GorillaStack,ParameterValue=$GorillaStack \
+ParameterKey=SNSTopicARN,ParameterValue=$SNSTopicARN ParameterKey=JoinDomain,ParameterValue=$JoinDomain \
+ParameterKey=Proxy,ParameterValue=$Proxy" 
 FinalStatus=`/root/.local/lib/aws/bin/aws cloudformation describe-stacks --stack-name deploy-moh-cli-test-amapoc-moh-private-1 |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
 echo $FinalStatus
