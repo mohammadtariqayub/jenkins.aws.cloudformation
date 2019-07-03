5b004829629e61b5caf8a97f8a63082832f439b7
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-03 22:00:50 +1000

    adding Jenkinsfile-job-waf.groovy

--- scripts/acm-cert-CD.sh
+++ scripts/acm-cert-CD.sh
@@ -26,7 +26,7 @@ do
     echo $FinalStatus
 done
 # Get ACM ID
-acm_arn_ap_southeast_2 = `/root/.local/lib/aws/bin/aws cloudformation describe-stack-resources --stack-name $StackName |grep PhysicalResourceId |cut -d '"' -f4`
-filename = acm_arn_ap_southeast_2_$StackName
+acm_arn_ap_southeast_2=`/root/.local/lib/aws/bin/aws cloudformation describe-stack-resources --stack-name $StackName |grep PhysicalResourceId |cut -d '"' -f4`
+filename=acm_arn_ap_southeast_2_$StackName
 echo ACM ARN ap-southeast-2 is $acm_arn_ap_southeast_2
 echo $acm_arn_ap_southeast_2 > /root/artifacts/$filename.txt
