e6b2e44c5abab8533fd5979da5c87d49790d56d4
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-03 19:31:00 +1000

    adding Jenkinsfile-job-waf.groovy

--- Jenkinsfile-job-waf.groovy
+++ Jenkinsfile-job-waf.groovy
@@ -1,6 +1,6 @@
 node ('slave') {
   
-  parameters {
+parameters {
         string(defaultValue: "deploy-moh-cli-test-amapoc-moh-private-web-3", description: 'Enter Stack Name', name: 'StackName')
         string(defaultValue: "subnet-061b3d9a7f8b88210", description: 'Enter Subnet ID', name: 'SubnetId')
         string(defaultValue: "sg-0accba28318e4819c", description: 'Enter Security Group', name: 'SecurityGroupIds')
@@ -9,15 +9,8 @@ node ('slave') {
         string(defaultValue: "windows", description: 'Enter OS', name: 'OS')
         string(defaultValue: "amapocmoh-4", description: 'Enter Host Name', name: 'HostName')
         string(defaultValue: "arn:aws:sns:ap-southeast-2:820636345429:mohammad-ping-poc-topic", description: 'Enter SNS Topics', name: 'SNSTopicARN')
-        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'AWSBucketName')
+        string(defaultValue: "mohammad-resize-dest", description: 'Enter Bucket Name', name: 'BucketName')
     }
-
-  stage ('parameter_check') {
-    def validAWSBucketName = (params['AWSBucketName'] ==~ /^(?!\s*$).+/)
-    if (!validAWSBucketName) { error "Invalid parameter Bucket Name. Parameter cannot be empty!" }
-    
-    echo "parameter_check stage complete!!!"
-  }
 
   stage('template copy'){
     git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
