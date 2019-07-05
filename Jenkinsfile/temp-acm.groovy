e34c06fab6b543f65630b65fb70d5fc5b6c649f8
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-05 10:09:13 +1000

    cloudfront-elb-security-groups.yml

--- Jenkinsfile/temp-acm.groovy
+++ Jenkinsfile/temp-acm.groovy
@@ -31,19 +31,4 @@ node ('slave') {
                 string(name: 'SystemOwner', value: params['SystemOwner'])
            ]
   }
-  stage('deploy security group') {
-    echo "deploying security group"
-    build job: 'cloudfront-sg-CD',
-           parameters: [
-                string(name: 'StackName', value: params['StackName']),
-                string(name: 'Application', value: params['Application']),
-                string(name: 'AWSAccountSSMParameter', value: params['AWSAccountSSMParameter']),
-                string(name: 'Comments', value: params['Comments']),
-                string(name: 'DeploymentBucket', value: params['DeploymentBucket']),
-                string(name: 'ProjectCode', value: params['ProjectCode']),
-                string(name: 'Environment', value: params['Environment']),
-                string(name: 'VpcId', value: params['VpcId']),
-                string(name: 'SystemOwner', value: params['SystemOwner'])
-           ]
-  }
 }
