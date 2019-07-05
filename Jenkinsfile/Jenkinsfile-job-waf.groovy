32cfb15f702b4528f6ab4af32031452301e89eff
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-05 17:38:10 +1000

    adding alb scripts

--- Jenkinsfile/Jenkinsfile-job-waf.groovy
+++ Jenkinsfile/Jenkinsfile-job-waf.groovy
@@ -31,6 +31,7 @@ node ('slave') {
                 string(name: 'SystemOwner', value: params['SystemOwner'])
            ]
   }
+
   stage('deploy security group') {
     echo "deploying security group"
     build job: 'cloudfront-sg-CD',
@@ -43,6 +44,29 @@ node ('slave') {
                 string(name: 'ProjectCode', value: params['ProjectCode']),
                 string(name: 'Environment', value: params['Environment']),
                 string(name: 'VpcId', value: params['VpcId']),
+                string(name: 'SystemOwner', value: params['SystemOwner'])
+           ]
+  }
+  
+    stage('deploy alb') {
+    echo "deploying alb"
+    build job: 'alb-CD',
+           parameters: [
+                string(name: 'StackName', value: params['StackName']),
+                string(name: 'ALBScheme', value: params['ALBScheme']),
+                string(name: 'ALBAcessLogBucket', value: params['ALBAcessLogBucket']),
+                string(name: 'VpcId', value: params['VpcId']),
+                string(name: 'PublicSubnetA', value: params['PublicSubnetA']),
+                string(name: 'PublicSubnetB', value: params['PublicSubnetB']),
+                string(name: 'PublicSubnetC', value: params['PublicSubnetC']),
+                string(name: 'EC2Instance1', value: params['EC2Instance1']),
+                string(name: 'EC2Instance2', value: params['EC2Instance2']),
+                string(name: 'Application', value: params['Application']),
+                string(name: 'BusinessOwner', value: params['BusinessOwner']),
+                string(name: 'BusinessUnit', value: params['BusinessUnit']),
+                string(name: 'Criticality', value: params['Criticality']),
+                string(name: 'Environment', value: params['Environment']),
+                string(name: 'RecID', value: params['RecID']),
                 string(name: 'SystemOwner', value: params['SystemOwner'])
            ]
   }
