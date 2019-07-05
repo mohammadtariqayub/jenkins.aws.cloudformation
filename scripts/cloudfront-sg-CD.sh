c1346e317260faae95b87087395483e73753c878
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-05 13:55:12 +1000

    modifying cloudfront-sg-CD.sh

--- scripts/cloudfront-sg-CD.sh
+++ scripts/cloudfront-sg-CD.sh
@@ -18,7 +18,7 @@ ParameterKey=SystemOwner,ParameterValue=$SystemOwner
 FinalStatus=`/root/.local/lib/aws/bin/aws cloudformation describe-stacks --stack-name $StackName_sg |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
 while [ "$FinalStatus" != "CREATE_COMPLETE" ]
 do
-    sleep 60
+    sleep 30
     FinalStatus=`/root/.local/lib/aws/bin/aws cloudformation describe-stacks --stack-name $StackName_sg |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
     echo $FinalStatus
 done
@@ -31,11 +31,32 @@ echo "`/root/.local/lib/aws/bin/aws cloudformation describe-stack-resources --st
 # Get lambda function ID
 lambda_ID=`/root/.local/lib/aws/bin/aws cloudformation describe-stack-resources --stack-name $StackName_sg |grep PhysicalResourceId |grep function |cut -d '"' -f4`
 filename_lambda="lambda_function_$StackName_sg"
-echo lambda function created is $lambda_ID
+echo lambda function is $lambda_ID
 echo $lambda_ID > /root/artifacts/$filename_lambda.txt
 
 # Get lambda ARN
-lambda_ARN=`/root/.local/lib/aws/bin/aws lambda get-function --function-name $lambda_ID |grep FunctionArn |cut -d '"' -f4`
+LambdaARN=`/root/.local/lib/aws/bin/aws lambda get-function --function-name $lambda_ID |grep FunctionArn |cut -d '"' -f4`
 filename_lambda_arn="lambda_arn_$StackName_sg"
-echo lambda ARN is $lambda_ARN
-echo $lambda_ID > /root/artifacts/$filename_lambda_arn.txt
+echo lambda ARN is $LambdaARN
+echo $LambdaARN > /root/artifacts/$filename_lambda_arn.txt
+
+echo "Security group and Lambda function creation done"
+
+# Subscribe to AmazonIpSpaceChanged Topic
+StackName_sns="$StackName-sns-$Application"
+echo "deploying Stack $StackName_sns"
+
+/root/.local/lib/aws/bin/aws cloudformation create-stack --stack-name $StackName_sns \
+--template-body file:///root/jenkins.aws.cloudformation/cloudformation/SNS-AmazonIpSpaceChanged-topic.yml --parameters \
+ParameterKey=LambdaARN,ParameterValue=$LambdaARN 
+
+# Wait for stack to complete
+FinalStatus=`/root/.local/lib/aws/bin/aws cloudformation describe-stacks --stack-name $StackName_sns |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
+while [ "$FinalStatus" != "CREATE_COMPLETE" ]
+do
+    sleep 30
+    FinalStatus=`/root/.local/lib/aws/bin/aws cloudformation describe-stacks --stack-name $StackName_sns |grep StackStatus |cut -d ":" -f2 |sed 's/[", ]//g'`
+    echo $FinalStatus
+done
+
+echo "SNS subscription for Lambda is done"
