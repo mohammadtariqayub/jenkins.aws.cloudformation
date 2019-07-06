0059ac9f922d3ffb6006db8ba64889d321a54a77
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-06 12:57:02 +1000

    adding was

--- scripts/waf-CD.sh
+++ scripts/waf-CD.sh
@@ -2,19 +2,33 @@
 # deploying WAF
 StackName_waf="$StackName-$Application-waf"
 echo "deploying Stack $StackName_waf"
+echo $StackName
+echo $Application
+echo $BusinessOwner
 
 #Defining parameters
 AppAccessLogBucket="$BusinessOwner-$Application-waf-logs"
+echo $AppAccessLogBucket
 ActivateSqlInjectionProtectionParam="yes"
+echo $ActivateSqlInjectionProtectionParam
 ActivateCrossSiteScriptingProtectionParam="yes"
+echo $ActivateCrossSiteScriptingProtectionParam
 ActivateHttpFloodProtectionParam="yes - AWS WAF rate based rule"
+echo $ActivateHttpFloodProtectionParam
 ActivateScannersProbesProtectionParam="yes - AWS Lambda log parser"
+echo $ActivateScannersProbesProtectionParam
 ActivateBadBotProtectionParam="yes"
+echo $ActivateBadBotProtectionParam
 EndpointType="CloudFront"
+echo $EndpointType
 RequestThreshold="2000"
+echo $RequestThreshold
 ErrorThreshold="50"
+echo $ErrorThreshold
 WAFBlockPeriod="240"
+echo $WAFBlockPeriod
 ApiGatewayBadBotName="$Application-$BusinessOwner"
+echo $ApiGatewayBadBotName
 
 /root/.local/lib/aws/bin/aws cloudformation create-stack --stack-name $StackName_waf \
 --template-body file:///root/jenkins.aws.cloudformation/cloudformation/deploy-waf-v2.3.yml --parameters \
