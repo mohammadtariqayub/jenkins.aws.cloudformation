0a98b92badba8ff213b7a56d001bddb6da0fc26b
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-06 11:57:47 +1000

    adding was

--- Jenkinsfile/Jenkinsfile-job-waf.groovy
+++ Jenkinsfile/Jenkinsfile-job-waf.groovy
@@ -41,7 +41,7 @@ node ('slave') {
            ]
   }
   
-    stage('deploy alb') {
+  stage('deploy alb') {
     echo "deploying alb"
     build job: 'alb-CD',
            parameters: [
@@ -63,4 +63,21 @@ node ('slave') {
                 string(name: 'SystemOwner', value: params['SystemOwner'])
            ]
   }
+
+  stage('deploy s3 buckets') {
+    echo "deploy s3 buckets for WAF and CF"
+    build job: 's3-bucket-CD',
+           parameters: [
+                string(name: 'StackName', value: params['StackName']),
+                string(name: 'Application', value: params['Application']),
+                string(name: 'EnvironmentType', value: params['EnvironmentType']),
+                string(name: 'BusinessOwner', value: params['BusinessOwner']),
+                string(name: 'BusinessUnit', value: params['BusinessUnit']),
+                string(name: 'Environment', value: params['Environment']),
+                string(name: 'ProjectCode', value: params['ProjectCode']),
+                string(name: 'RFC', value: params['RFC']),
+                string(name: 'SystemOwner', value: params['SystemOwner'])
+           ]
+  }
+
 }
